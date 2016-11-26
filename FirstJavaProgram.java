0239afc4d63a5e529a46412c59d2cce12e83a353
Prashant Pandey <prash22face@gmail.com>
2016-11-26 21:43:31 +0500

    Hello world program in Java

---
+++ FirstJavaProgram.java
@@ -0,0 +1,8 @@
+// by default java.lang package is imported
+// Class name starts from Capital letter conventionally
+class MyFirstProgram{
+        // main method execute
+        public static void main(String []args){
+            System.out.print("HELLO JAVA");
+        }
+}
